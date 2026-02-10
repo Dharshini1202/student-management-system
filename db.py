1f9cec36148f7ba57b44139c4d76931cae83d3a3
Dharshini Priya P <dharshi.panbu@gmail.com>
2026-02-10 19:35:32 +0530

    Add .gitignore and remove IDE/cache files; secure DB config

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Dharsh@12",
-        database="student_management"
-    )
+load_dotenv()
+
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
